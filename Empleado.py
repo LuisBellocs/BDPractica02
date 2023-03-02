ea7cd133331b5221f6bf270d1fc010be283f7bc4
unknown <luis_bello@ciencias.unam.mx>
2023-03-02 01:04:18 -0600

    addicion clase empleado

---
+++ Empleado.py
@@ -0,0 +1,11 @@
+class Empleado:
+
+    def __init__(self, primer_nombre, apellido_paterno, apellido_materno, direccion, correos_electronicos, telefonos, fecha_nacimiento):
+
+        self.primer_nombre = primer_nombre
+        self.apellio_paterno = apellido_paterno
+        self.apellido_materno = apellido_materno
+        self.direccion = direccion
+        self.correos_electronicos = correos_electronicos
+        self.telefonos = telefonos
+        self.fecha_nacimiento = fecha_nacimiento
