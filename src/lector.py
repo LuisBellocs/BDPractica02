0e01a60bda3a8bbdf3b5a41df9a01c82b1394265
Arturo González Bretón <arturo.gbreton@ciencias.unam.mx>
2023-03-03 16:28:36 -0600

    Delete Empleados.csv lector.py main.py

--- src/lector.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def write_to_csv(filename, data):
-
-    with open(filename, 'a', newline='') as file:
-        writer = csv.writer(file)
-
-        for row in data:
-            writer.writerow(row)
