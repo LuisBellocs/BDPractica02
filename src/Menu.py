543f8d244d9f3a1314de599abb676363069ba803
Arturo González Bretón <arturo.gbreton@ciencias.unam.mx>
2023-03-03 16:25:05 -0600

    Add clase Menu

---
+++ src/Menu.py
@@ -0,0 +1,23 @@
+class Menu:
+    @staticmethod
+    def menuAccion():
+        print("Elija una acción:")
+        print("1) Agregar")
+        print("2) Consultar")
+        print("3) Editar")
+        print("4) Eliminar")
+
+    @staticmethod
+    def menuEntidad():
+        print("Elija una entidad:")
+        print("1) Empleados")
+        print("2) Productos")
+        print("3) Sucursales")
+
+    @staticmethod
+    def menuID():
+        print("Escriba el ID: ")
+
+    @staticmethod
+    def menuError():
+        print("Esa no es una opción válida. Elija de nuevo.")
