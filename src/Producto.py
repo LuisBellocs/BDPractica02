67d797a0eda43b40a86968fba0486e1503c96f16
Arturo González Bretón <arturo.gbreton@ciencias.unam.mx>
2023-03-03 16:26:57 -0600

    Modify clase producto

--- src/Producto.py
+++ src/Producto.py
@@ -1,13 +1,47 @@
+import csv
+
 class Producto:
 
-    def __init__(self, precio, cantidad, nombre, marca, presentacion, cantidad_stock, requiere_refrigeracion, fecha_elaboracion, fecha_caducidad):
+    @staticmethod
+    def agrega():
+        productos = open('productos.csv', 'a')
+        
+        id = input("código de producto: \n")
+        precio = input("Precio: \n")
+        cantidad = input("Cantidad total de productos: \n")
+        nombre = input("Nombre: \n")
+        marca = input("Presentación: \n")
+        tiendas = int(input("Número de tiendas con el producto: \n"))
+        stock = ""
+        for i in range(0,tiendas):
+            stock += input("Nombre de la tienda {}: \n".format(i))
+            stock += ":"
+            stock += input("Cantidad en la tienda {}: \n".format(i+1))
+        refrigeracion = input("Requiere refrigeración (si/no): \n")
+        preparación = input("Fecha de preparación o elaboración (dd/mm/aaaa): \n")
+        caducidad = input("Fecha de caducidad (dd/mm/aaaa): \n")
 
-        self.precio = precio
-        self.cantidad = cantidad
-        self.nombre = nombre
-        self.marca = marca
-        self.presentacion = presentacion
-        self.cantidad_stock = cantidad_stock
-        self.requiere_refrigeracion = requiere_refrigeracion
-        self.fecha_elaboracion = fecha_elaboracion
-        self.fecha_caducidad = fecha_caducidad
+        with productos:
+            writer = csv.writer(productos, delimiter=',')
+            writer.writerow([id, precio, cantidad, nombre,
+                    marca, tiendas, stock, refrigeracion,
+                    preparación, caducidad])
+            
+        productos.close()
+    
+    @staticmethod
+    def edita(id):
+        productos = open("productos.csv")
+        with productos:
+            reader = csv.reader(productos, delimiter = ',')
+            close()
+                #TODO implementar método
+    
+    @staticmethod
+    def elimina(id):
+        productos = open("productos.csv")
+        with productos:
+            reader = csv.reader(productos, delimiter = ',')
+            close()
+                #TODO implementar método
+            
