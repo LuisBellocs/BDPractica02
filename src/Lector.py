41c52a5621eaced626a066425f9e81d18cf99910
edulol-1 <edu3000@ciencias.unam.mx>
2023-03-04 17:11:15 -0600

    Change metodo lee en clase Lector

--- src/Lector.py
+++ src/Lector.py
@@ -1,24 +1,23 @@
 import csv
 
 class Lector:
+
+    @staticmethod
+    def imprime_fila_entidad(cadenaEntidad, id):
+        with open(f'{cadenaEntidad}.csv') as entidad:
+            reader = csv.reader(entidad) 
+            for fila in reader:
+                if (fila[0] == id):
+                    print(fila)
+    
     @staticmethod
     def lee(entidad, id):
-
+        cadenaEntidad = ""
         if (entidad == 1):
-            with open('empleados.csv') as empleados:
-                reader = csv.reader(empleados)
-                for empleado in reader:
-                    if (empleado[0] == id):
-                        print(empleado)
+            cadenaEntidad = "empleados"
         elif (entidad == 2):
-            with open('productos.csv') as productos:
-                reader = csv.reader(productos)
-                for producto in reader:
-                    if (producto[0] == id):
-                        print(producto)
+            cadenaEntidad = "productos"
         else:
-            with open('sucursales.csv') as sucursales:
-                reader = csv.reader(sucursales)
-                for sucursal in reader:
-                    if (sucursal[0] == id):
-                        print(sucursal)
+            cadenaEntidad = "sucursales"
+        Lector.imprime_fila_entidad(cadenaEntidad, id)
+        
