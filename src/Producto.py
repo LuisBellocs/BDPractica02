5109554ef30ec89f580a59f7ba64f70a777b713e
edulol-1 <edu3000@ciencias.unam.mx>
2023-03-05 14:16:57 -0600

    Modify clase Producto, reduccion de codigo

--- src/Producto.py
+++ src/Producto.py
@@ -14,7 +14,7 @@ class Producto:
         tiendas = int(input("Número de tiendas con el producto: \n"))
         stock = ""
         for i in range(0,tiendas):
-            stock += input("Nombre de la tienda {}: \n".format(i))
+            stock += input("Nombre de la tienda {}: \n".format(i+1))
             stock += ":"
             stock += input("Cantidad en la tienda {}: \n".format(i+1))
         refrigeracion = input("Requiere refrigeración (si/no): \n")
@@ -31,12 +31,22 @@ class Producto:
     
     @staticmethod
     def edita(id):
+        Producto.debe_editar = True
+        Producto.decide_edita_elimina(id)
+    
+    @staticmethod
+    def elimina(id):
+        Producto.debe_editar = False
+        Producto.decide_edita_elimina(id)
+
+    @staticmethod
+    def decide_edita_elimina(id):
         listaProductos = []
         with open('productos.csv', 'r+') as productos:
                 reader = csv.reader(productos)
                 writer = csv.writer(productos, delimiter=',')
                 for producto in reader:
-                    if (producto[0] == id):
+                    if (producto[0] == id and Producto.debe_editar):
                         id = input("código de producto: \n")
                         precio = input("Precio: \n")
                         cantidad = input("Cantidad total de productos: \n")
@@ -45,7 +55,7 @@ class Producto:
                         tiendas = int(input("Número de tiendas con el producto: \n"))
                         stock = ""
                         for i in range(0,tiendas):
-                            stock += input("Nombre de la tienda {}: \n".format(i))
+                            stock += input("Nombre de la tienda {}: \n".format(i+1))
                             stock += ":"
                             stock += input("Cantidad en la tienda {}: \n".format(i+1))
                         refrigeracion = input("Requiere refrigeración (si/no): \n")
@@ -54,21 +64,6 @@ class Producto:
                         listaProductos.append([id, precio, cantidad, nombre,
                             marca, tiendas, stock, refrigeracion,
                             preparación, caducidad])
-                    else:
-                        listaProductos.append(producto)
-
-        nuevosProductos = open('productos.csv', 'w')
-        with nuevosProductos:
-            writer = csv.writer(nuevosProductos)
-            writer.writerows(listaProductos)
-    
-    @staticmethod
-    def elimina(id):
-        listaProductos = []
-        with open('productos.csv', 'r+') as productos:
-                reader = csv.reader(productos)
-                writer = csv.writer(productos, delimiter=',')
-                for producto in reader:
                     if (producto[0] != id):
                         listaProductos.append(producto)
 
@@ -76,4 +71,4 @@ class Producto:
         with nuevosProductos:
             writer = csv.writer(nuevosProductos)
             writer.writerows(listaProductos)
-            
+        
