31e2d896785df6fe597dfed2672f536faee166a6
edulol-1 <edu3000@ciencias.unam.mx>
2023-03-05 14:09:47 -0600

    Modify clase Empleado, reduccion de codigo

--- src/Empleado.py
+++ src/Empleado.py
@@ -2,6 +2,8 @@ import csv
 
 class Empleado:
 
+    debe_editarse = False
+    
     @staticmethod
     def agrega():
         empleados = open('empleados.csv', 'a')
@@ -12,48 +14,46 @@ class Empleado:
         correos = input("Correos (separados por ';'): \n")
         celulares = input("Números de celular (separados por ';'): \n")
         nacimiento = input("Fecha de nacimiento (dd/mm/aaaa): \n")
-                
+        
         with empleados:
             writer = csv.writer(empleados, delimiter=',')
             writer.writerow([curp, nombre, direccion, correos,
-                                                celulares, nacimiento])
-        
+                             celulares, nacimiento])
+            
         empleados.close()
 
+        
     @staticmethod
     def edita(id):
-        listaEmpleados = []
-        with open('empleados.csv', 'r+') as empleados:
-                reader = csv.reader(empleados)
-                writer = csv.writer(empleados, delimiter=',')
-                for empleado in reader:
-                    if (empleado[0] == id):
-                        curp = input("CURP: \n")
-                        nombre = input("Nombre: \n")
-                        direccion = input("Dirección: \n")
-                        correos = input("Correos (separados por ';'): \n")
-                        celulares = input("Números de celular (separados por ';'): \n")
-                        nacimiento = input("Fecha de nacimiento (dd/mm/aaaa): \n")
-                        listaEmpleados.append([curp, nombre, direccion, correos,
-                            celulares, nacimiento])
-                    else:
-                        listaEmpleados.append(empleado)
-
-        nuevosEmpleados = open('empleados.csv', 'w')
-        with nuevosEmpleados:
-            writer = csv.writer(nuevosEmpleados)
-            writer.writerows(listaEmpleados)
+        Empleado.debe_editarse = True
+        Empleado.elimina_o_edita(id)
+            
     @staticmethod
     def elimina(id):
+        Empleado.debe_editarse = False
+        Empleado.elimina_o_edita(id)
+
+    @staticmethod
+    def elimina_o_edita(id):
         listaEmpleados = []
         with open('empleados.csv', 'r+') as empleados:
-                reader = csv.reader(empleados)
-                writer = csv.writer(empleados, delimiter=',')
-                for empleado in reader:
-                    if (empleado[0] != id):
-                        listaEmpleados.append(empleado)
+            reader = csv.reader(empleados)
+            writer = csv.writer(empleados, delimiter=',')
+            for empleado in reader:
+                if (empleado[0] == id and Empleado.debe_editarse):
+                    curp = input("CURP: \n")
+                    nombre = input("Nombre: \n")
+                    direccion = input("Dirección: \n")
+                    correos = input("Correos (separados por ';'): \n")
+                    celulares = input("Números de celular (separados por ';'): \n")
+                    nacimiento = input("Fecha de nacimiento (dd/mm/aaaa): \n")
+                    listaEmpleados.append([curp, nombre, direccion, correos,
+                                           celulares, nacimiento])
+                if (empleado[0] != id):
+                    listaEmpleados.append(empleado)
 
         nuevosEmpleados = open('empleados.csv', 'w')
         with nuevosEmpleados:
             writer = csv.writer(nuevosEmpleados)
             writer.writerows(listaEmpleados)
+        
