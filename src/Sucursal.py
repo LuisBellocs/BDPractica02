4748ccc01c9921b3e6f9d5f0392024f33b10becb
edulol-1 <edu3000@ciencias.unam.mx>
2023-03-05 14:46:41 -0600

    Modify calse Sucursal para reduccion de codigo

--- src/Sucursal.py
+++ src/Sucursal.py
@@ -2,6 +2,8 @@ import csv
 
 class Sucursal:
 
+    debe_editarse = False
+    
     @staticmethod
     def agrega():
         sucursales = open('sucursales.csv', 'a')
@@ -21,12 +23,22 @@ class Sucursal:
         
     @staticmethod
     def edita(id):
+        Sucursal.debe_editarse = True
+        Sucursal.elimina_o_edita(id)
+    
+    @staticmethod
+    def elimina(id):
+        Sucursal.debe_editarse = False
+        Sucursal.selimina_o_edita(id)            
+
+    @staticmethod
+    def elimina_o_edita(id):
         listaSucursales = []
         with open('sucursales.csv', 'r+') as sucursales:
                 reader = csv.reader(sucursales)
                 writer = csv.writer(sucursales, delimiter=',')
                 for sucursal in reader:
-                    if (sucursal[0] == id):
+                    if (sucursal[0] == id and Sucursal.debe_editarse):
                         id = input("código de sucursal: \n")
                         nombre = input("Nombre: \n")
                         direccion = input("Dirección: \n")
@@ -34,21 +46,6 @@ class Sucursal:
                         apertura = input("Fecha de apertura (dd/mm/aaaa): \n")
                         listaSucursales.append([id, nombre, direccion,
                             telefonos, apertura])
-                    else:
-                        listaSucursales.append(sucursal)
-
-        nuevasSucursales = open('sucursales.csv', 'w')
-        with nuevasSucursales:
-            writer = csv.writer(nuevasSucursales)
-            writer.writerows(listaSucursales)
-    
-    @staticmethod
-    def elimina(id):
-        listaSucursales = []
-        with open('sucursales.csv', 'r+') as sucursales:
-                reader = csv.reader(sucursales)
-                writer = csv.writer(sucursales, delimiter=',')
-                for sucursal in reader:
                     if (sucursal[0] != id):
                         listaSucursales.append(sucursal)
 
@@ -56,5 +53,4 @@ class Sucursal:
         with nuevasSucursales:
             writer = csv.writer(nuevasSucursales)
             writer.writerows(listaSucursales)
-            
-            
+        
