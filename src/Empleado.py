21ba7a915cd48cbf0d525ea13d26275075144a86
unknown <luis_bello@ciencias.unam.mx>
2023-03-05 23:09:45 -0600

    actualizacion Empleado y Producto

---
+++ src/Empleado.py
@@ -0,0 +1,126 @@
+import csv
+
+class Empleado:
+
+    debe_editarse = False
+    
+    @staticmethod
+    def agrega():
+        empleados = open('empleados.csv', 'a')
+        with empleados:
+            writer = csv.writer(empleados, delimiter=',')
+            writer.writerow(Empleado.get_datos())
+        empleados.close()
+        
+    @staticmethod
+    def edita(id):
+        Empleado.debe_editarse = True
+        Empleado.elimina_o_edita(id)
+            
+    @staticmethod
+    def elimina(id):
+        Empleado.debe_editarse = False
+        Empleado.elimina_o_edita(id)
+
+    @staticmethod
+    def elimina_o_edita(id):
+        listaEmpleados = []
+        with open('empleados.csv', 'r+') as empleados:
+            reader = csv.reader(empleados)
+            writer = csv.writer(empleados, delimiter=',')
+            for empleado in reader:
+                if (empleado[0] == id and Empleado.debe_editarse):
+                    listaEmpleados.append(Empleado.get_datos())
+                if (empleado[0] != id):
+                    listaEmpleados.append(empleado)
+
+        nuevosEmpleados = open('empleados.csv', 'w')
+        with nuevosEmpleados:
+            writer = csv.writer(nuevosEmpleados)
+            writer.writerows(listaEmpleados)
+
+    @staticmethod
+    def get_datos():
+        curp = Empleado.checarCurp()
+        nombre = Empleado.checarNombre()
+        direccion = input("Dirección: \n")
+        correos = Empleado.checarEmail()
+        celulares = Empleado.checarTel()
+        nacimiento = Empleado.checarFecha()
+        sucursal = input("Agrega Sucursal: \n") #Implementar la imposibilidad de tener un empleado en dos sucursales
+        return [curp, nombre, direccion, correos,
+                celulares, nacimiento, sucursal]
+        
+
+    def checarCurp():
+        while True:
+            curp = input("Ingrese su CURP: ")
+            
+            if len(curp) != 18:
+                print("La CURP debe tener 18 caracteres.")
+                continue
+            
+            for i in range(18):
+                c = curp[i]
+                if(i < 4 or (i >= 10 and i <= 16)):                    
+                    if not c.isalpha() or not c.isupper(): # letras mayúsculas
+                        print("La CURP debe contener letras mayúsculas en las posiciones 1, 2, 3, 4 y 11.")
+                        break
+                elif(i == 10):
+                    if c != 'H' and c != 'M':
+                        print("La CURP debe contener H o M en la posicíon 11.")
+                        break
+                else:
+                    if(not c.isdigit()):
+                        print("Los caracteres del 5 al 10 y el 18 deben de ser números")
+                        break
+            else:
+                return curp
+            
+    def checarFecha():
+        dato = ""
+        while(dato == ""):
+            dato = input("Fecha de nacimiento (dd/mm/aaaa): \n")
+            numfecha = dato.split("/")
+            try:
+                fecha = datetime.datetime(int(numfecha[2]), int(numfecha[1]), int(numfecha[0]))
+            except:
+                print("Formato de fecha inválido, intente de nuevo.")
+                dato = ""
+        return dato
+    
+    def checarTel():
+        dato = ""
+        while(dato == ""):
+            dato = input("Número(s) de teléfono (separados por ';'): \n")
+            telefonos = dato.split(";")
+            for numero in telefonos:
+                if(len(numero) < 7):
+                    print("Alguno de los datos es inválido, intente de nuevo.")
+                    dato = ""
+                try:
+                    num = int(numero)
+                except:
+                    print("Alguno de los datos es inválido, intente de nuevo.")
+                    dato = ""
+        return dato
+    
+    def checarEmail():
+        dato = ""
+        while(dato == ""):
+            dato = input("Correo(s) electríonico(s) (separados por ';'): \n")
+            emails = dato.split(";")
+            for email in emails:
+                if((len(email) < 3) or ("@" not in email) or (email[1] == "@") or (email[-1] == "@")):
+                    print("Alguno de los datos es inválido, intente de nuevo.")
+                    dato = ""
+                
+        return dato
+    
+    def checarNombre():
+        while True:
+            nombre = input("Nombre completo: ")
+            if(all(c.isalpha() or c.isspace() for c in nombre) and len(nombre) > 2 and " " in nombre and (nombre[1] != " ") and (nombre[-1] != " ")):
+                return nombre
+            else:
+                print("El formato del nombre es incorrecto. Inténtelo de nuevo.")
--- src/Producto.py
+++ src/Producto.py
@@ -1,13 +1,134 @@
+import csv
+import datetime
+
 class Producto:
 
-    def __init__(self, precio, cantidad, nombre, marca, presentacion, cantidad_stock, requiere_refrigeracion, fecha_elaboracion, fecha_caducidad):
+    debe_editar = False
 
-        self.precio = precio
-        self.cantidad = cantidad
-        self.nombre = nombre
-        self.marca = marca
-        self.presentacion = presentacion
-        self.cantidad_stock = cantidad_stock
-        self.requiere_refrigeracion = requiere_refrigeracion
-        self.fecha_elaboracion = fecha_elaboracion
-        self.fecha_caducidad = fecha_caducidad
+    @staticmethod
+    def agrega():
+        productos = open('productos.csv', 'a')
+        with productos:
+            writer = csv.writer(productos, delimiter=',')
+            writer.writerow(Producto.get_datos())
+            
+        productos.close()
+    
+    @staticmethod
+    def edita(id):
+        Producto.debe_editar = True
+        Producto.decide_edita_elimina(id)
+    
+    @staticmethod
+    def elimina(id):
+        Producto.debe_editar = False
+        Producto.decide_edita_elimina(id)
+
+    @staticmethod
+    def decide_edita_elimina(id):
+        listaProductos = []
+        with open('productos.csv', 'r+') as productos:
+                reader = csv.reader(productos)
+                writer = csv.writer(productos, delimiter=',')
+                for producto in reader:
+                    if (producto[0] == id and Producto.debe_editar):
+                        listaProductos.append(Producto.get_datos())
+                    if (producto[0] != id):
+                        listaProductos.append(producto)
+
+        nuevosProductos = open('productos.csv', 'w')
+        with nuevosProductos:
+            writer = csv.writer(nuevosProductos)
+            writer.writerows(listaProductos)
+
+    @staticmethod
+    def get_datos():
+        id = Producto.checarID()
+        precio = Producto.checarPrecio()
+        cantidad = Producto.checarInt()
+        nombre = input("Nombre: \n")
+        marca = input("Nombre de la marca: \n")
+        tiendas = int(input("Número de tiendas con el producto: \n")) #Checar?
+        stock = ""
+        for i in range(0,tiendas):
+            stock += input("Nombre de la tienda {}: \n".format(i+1))
+            stock += ":"
+            stock += input("Cantidad en la tienda {}: \n".format(i+1))
+        refrigeracion = Producto.checarRefrigereacion()
+        preparación = Producto.checarFecha("preparación")
+        caducidad = Producto.checarFecha("caducidad")
+        return [id, precio, cantidad, nombre, marca, tiendas, 
+            stock, refrigeracion, preparación, caducidad]
+        
+
+    @staticmethod
+    def checarID():
+        id = -1
+        while(id == -1):
+            try:
+                id = input("código de producto: \n")
+                numid = int(id)
+                if(numid < 0):
+                    print("ID inválido, intente de nuevo.")
+                    id = -1
+                else:
+                    repetido = Lector.lee(3, id)
+                    if(repetido != "" and repetido[0] == id and
+                        Producto.debe_editar == False):
+                        print("Ya existe un producto con ese código, intente de nuevo.")
+                        id = -1
+            except:
+                print("El código de producto debe ser numérico")
+                id = -1
+        return id
+
+    def checarFecha(str):
+        dato = ""
+        while(dato == ""):
+            dato = input("Fecha de " + str + " (dd/mm/aaaa): \n")
+            numfecha = dato.split("/")
+            try:
+                fecha = datetime.datetime(int(numfecha[2]), int(numfecha[1]), int(numfecha[0]))
+            except:
+                print("Formato de fecha inválido, intente de nuevo.")
+                dato = ""
+        return dato
+    
+    def checarRefrigeracion():
+        dato = ""
+        while(dato == ""):
+            dato = input("Requiere refrigeración (si/no): \n")
+            try:
+                if(dato != "si" and dato != "no"):
+                    print("Formato inválido, intente de nuevo.")
+                    dato = ""
+            except:
+                print("Formato inválido, intente de nuevo.")
+                dato = ""
+        return dato
+        
+    def checarPrecio():
+        pr = -1.1
+        while(pr == -1.1):
+            try:
+                pr = input("Precio del de producto: \n")
+                if(pr <= 0):
+                    print("Precio inválido, intente de nuevo.")
+                    pr = -1.1
+            except:
+                print("El código de producto debe ser numérico") #Agregar que debe ser numérico con punto decimal?
+                pr = -1.1
+        return pr
+    
+    def checarInt():
+        pr = -1
+        while(pr == -1):
+            try:
+                pr = input(": \n")
+                if(pr < 0):
+                    print("Precio inválido, intente de nuevo.")
+                    pr = -1
+            except:
+                print("Este valor debe de ser numérico")
+                pr = -1
+        return pr
