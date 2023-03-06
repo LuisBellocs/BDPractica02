fa1144147004581179b351cbe49c91164cccccd5
JoelReyes <joelhaiddreyescedillo@ciencias.unam.mx>
2023-03-05 23:54:19 -0600

    Corregidos métodos Empleado y Producto

--- src/Empleado.py
+++ src/Empleado.py
@@ -1,4 +1,6 @@
 import csv
+import datetime
+from Lector import Lector
 
 class Empleado:
 
@@ -42,12 +44,97 @@ class Empleado:
 
     @staticmethod
     def get_datos():
-        curp = input("CURP: \n")
-        nombre = input("Nombre: \n")
+        curp = Empleado.checarCurp()
+        nombre = Empleado.checarNombre()
         direccion = input("Dirección: \n")
-        correos = input("Correos (separados por ';'): \n")
-        celulares = input("Números de celular (separados por ';'): \n")
-        nacimiento = input("Fecha de nacimiento (dd/mm/aaaa): \n")
+        correos = Empleado.checarEmail()
+        celulares = Empleado.checarTel()
+        nacimiento = Empleado.checarFecha()
+        sucursal = input("Agrega Sucursal: \n") #Implementar la imposibilidad de tener un empleado en dos sucursales
         return [curp, nombre, direccion, correos,
-                celulares, nacimiento]
-        
+                celulares, nacimiento, sucursal]
+    
+    @staticmethod
+    def checarCurp():
+        curp = ""
+        while (curp == ""):
+            curp = input("Ingrese su CURP: ")
+            
+            if len(curp) != 18:
+                print("La CURP debe tener 18 caracteres.")
+                curp = ""
+
+            repetido = Lector.lee(1, id)
+            if(repetido != "" and repetido[0] == id and
+                Empleado.debe_editarse == False):
+                print("Ya existe un empleado con ese CURP, intente de nuevo.")
+                curp = ""
+            
+            for i in range(18):
+                c = curp[i]
+                if(i < 4 or (i >= 10 and i <= 16)):                    
+                    if not c.isalpha() or not c.isupper():
+                        print("La CURP debe contener letras mayúsculas en las posiciones 1, 2, 3, 4 y 11.")
+                        curp = ""
+                elif(i == 10):
+                    if c != 'H' and c != 'M':
+                        print("La CURP debe contener H o M en la posicíon 11.")
+                        curp = ""
+                else:
+                    if(not c.isdigit()):
+                        print("Los caracteres del 5 al 10 y el 18 deben de ser números.")
+                        curp = ""
+
+        return curp
+    
+    @staticmethod
+    def checarFecha():
+        dato = ""
+        while(dato == ""):
+            dato = input("Fecha de nacimiento (dd/mm/aaaa): \n")
+            numfecha = dato.split("/")
+            try:
+                fecha = datetime.datetime(int(numfecha[2]), int(numfecha[1]), int(numfecha[0]))
+            except:
+                print("Formato de fecha inválido, intente de nuevo.")
+                dato = ""
+        return dato
+    
+    @staticmethod
+    def checarTel():
+        dato = ""
+        while(dato == ""):
+            dato = input("Número(s) de teléfono (separados por ';'): \n")
+            telefonos = dato.split(";")
+            for numero in telefonos:
+                if(len(numero) < 7):
+                    print("Alguno de los datos es inválido, intente de nuevo.")
+                    dato = ""
+                try:
+                    num = int(numero)
+                except:
+                    print("Alguno de los datos es inválido, intente de nuevo.")
+                    dato = ""
+        return dato
+    
+    @staticmethod
+    def checarEmail():
+        dato = ""
+        while(dato == ""):
+            dato = input("Correo(s) electrónico(s) (separados por ';'): \n")
+            emails = dato.split(";")
+            for email in emails:
+                if((len(email) < 3) or ("@" not in email) or (email[1] == "@") or (email[-1] == "@")):
+                    print("Alguno de los datos es inválido, intente de nuevo.")
+                    dato = ""
+                
+        return dato
+    
+    @staticmethod
+    def checarNombre():
+        while True:
+            nombre = input("Nombre completo: ")
+            if(all(c.isalpha() or c.isspace() for c in nombre) and len(nombre) > 2 and " " in nombre and (nombre[1] != " ") and (nombre[-1] != " ")):
+                return nombre
+            else:
+                print("El formato del nombre es incorrecto, intente de nuevo.")
--- src/Producto.py
+++ src/Producto.py
@@ -1,6 +1,10 @@
 import csv
+import datetime
+from Lector import Lector
 
 class Producto:
+
+    debe_editar = False
 
     @staticmethod
     def agrega():
@@ -40,20 +44,96 @@ class Producto:
 
     @staticmethod
     def get_datos():
-        id = input("código de producto: \n")
-        precio = input("Precio: \n")
-        cantidad = input("Cantidad total de productos: \n")
+        id = Producto.checarID()
+        precio = Producto.checarPrecio()
+        cantidad = Producto.checarInt()
         nombre = input("Nombre: \n")
-        marca = input("Presentación: \n")
-        tiendas = int(input("Número de tiendas con el producto: \n"))
+        marca = input("Nombre de la marca: \n")
+        tiendas = int(input("Número de tiendas con el producto: \n")) #Checar?
         stock = ""
         for i in range(0,tiendas):
             stock += input("Nombre de la tienda {}: \n".format(i+1))
             stock += ":"
             stock += input("Cantidad en la tienda {}: \n".format(i+1))
-        refrigeracion = input("Requiere refrigeración (si/no): \n")
-        preparación = input("Fecha de preparación o elaboración (dd/mm/aaaa): \n")
-        caducidad = input("Fecha de caducidad (dd/mm/aaaa): \n")
+        refrigeracion = Producto.checarRefrigeracion()
+        preparación = Producto.checarFecha("preparación")
+        caducidad = Producto.checarFecha("caducidad")
         return [id, precio, cantidad, nombre, marca, tiendas, 
-            stock, refrigeracion, preparación, caducidad]
+            stock, refrigeracion, preparación, caducidad]       
         
+    @staticmethod
+    def checarID():
+        id = -1
+        while(id == -1):
+            try:
+                id = input("código de producto: \n")
+                numid = int(id)
+                if(numid < 0):
+                    print("ID inválido, intente de nuevo.")
+                    id = -1
+                else:
+                    repetido = Lector.lee(2, id)
+                    if(repetido != "" and repetido[0] == id and
+                        Producto.debe_editar == False):
+                        print("Ya existe un producto con ese código, intente de nuevo.")
+                        id = -1
+            except:
+                print("El código de producto debe ser numérico.")
+                id = -1
+        return id
+
+    @staticmethod
+    def checarFecha(str):
+        dato = ""
+        while(dato == ""):
+            dato = input("Fecha de " + str + " (dd/mm/aaaa): \n")
+            numfecha = dato.split("/")
+            try:
+                fecha = datetime.datetime(int(numfecha[2]), int(numfecha[1]), int(numfecha[0]))
+            except:
+                print("Formato de fecha inválido, intente de nuevo.")
+                dato = ""
+        return dato
+    
+    @staticmethod
+    def checarRefrigeracion():
+        dato = ""
+        while(dato == ""):
+            dato = input("Requiere refrigeración (si/no): \n")
+            dato = dato.lower()
+            try:
+                if(dato != "si" and dato != "no"):
+                    print("Formato inválido, intente de nuevo.")
+                    dato = ""
+            except:
+                print("Formato inválido, intente de nuevo.")
+                dato = ""
+        return dato
+    
+    @staticmethod
+    def checarPrecio():
+        dato = -0.1
+        while(dato == -0.1):
+            try:
+                dato = input("Precio del de producto: \n")
+                if(dato <= 0):
+                    print("Precio inválido, intente de nuevo.")
+                    dato = -0.1
+            except:
+                print("El código de producto debe ser numérico.")
+                dato = -0.1
+        return dato
+    
+    @staticmethod
+    def checarInt():
+        dato = -1
+        while(dato == -1):
+            try:
+                dato = input(": \n")
+                if(dato < 0):
+                    print("Dato inválido, intente de nuevo.")
+                    dato = -1
+            except:
+                print("Este valor debe de ser numérico, intente de nuevo.")
+                dato = -1
+        return dato
